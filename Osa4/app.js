e2bc89161d7e3b98a840196f7425645f2870aa41
tetuanka <terhikamula@gmail.com>
2022-07-06 16:09:23 +0300

    Osa4

---
+++ Osa4/app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+logger.info('connecting to', url)
+
+mongoose.connect(config.MONGODB_URI)
+  .then( () => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+//require('dotenv').config()
+//const http = require('http')
+//const Blog = require('./models/blog')
+
+
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
